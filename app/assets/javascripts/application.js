bfce047624e83a5620a0436a09324803d20e538a
Adam Lucia <lucia.adam@gmail.com>
2017-03-08 05:07:54 +0000

    add bucket class

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//
+//= require buckets
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
--- app/assets/javascripts/buckets.js
+++ app/assets/javascripts/buckets.js
@@ -1,3 +1,17 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+class Bucket {
+  constructor(name, description = false, items = []) {
+    this.name = name
+    this.description = description;
+    this.items = items;
+  }
+
+  total_cost {
+    var moneyCost = 0;
+    var timeCost = 0;
+    this.items.forEach((item) => {
+      moneyCost += item.price;
+      timeCost += item.days_cost;
+    });
+    return `$${moneyCost} and ${timeCost} days needed to kick this bucket.`
+  }
+}
